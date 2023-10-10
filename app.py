a57c3095ea0c9ec2a90d11e44155ae999ac5dab1
Sam Murgatroyd <sammurgatroyd30@gmail.com>
2023-10-10 12:42:51 +0100

    test driving routes, chall complete commit

--- app.py
+++ app.py
@@ -16,6 +16,59 @@ app = Flask(__name__)
 def get_emoji():
     return ":)"
 
+@app.route('/hello', methods=['GET'])
+def get_ehllo():
+    return 'hello'
+
+@app.route('/goodbye', methods=['GET'])
+def goodbye():
+    name = request.args['name']
+    return f'Goodbye {name} !@'
+
+@app.route('/names', methods=['GET'])
+def names():
+    name_list = ['Julia', 'Alice', 'Karim',]
+    name = request.args['name']
+    print(name_list)
+    name_list.append(name)
+    print(name_list)
+    sep = ', '
+    return sep.join(name_list)
+    
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    message = request.form['message']
+    return f'Thanks {name}, you sent this message: {message}'
+@app.route('/wave', methods=['GET'])
+def wave():
+    name = request.args['name']
+    return f'I am waving at {name}'
+
+@app.route('/count_vowels', methods=['POST'])
+def count_vowels():
+    text = request.form['text']
+    number_of_vowels = []
+    for i in text:
+        if i in 'aeiou':
+            number_of_vowels.append(i)
+    result = len(number_of_vowels)
+    print(number_of_vowels)
+    return f'There are {result} vowels in "{text}"'
+
+@app.route('/sort-names', methods=['POST'])
+def sort_names():
+    names = request.form['names']
+    names_list = names.split(',')
+    sorted_list = sorted(names_list)
+    print(sorted_list)
+    sep = ','
+    return sep.join(sorted_list)
+
+
+
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
 from example_routes import apply_example_routes
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,67 @@ def test_get_emoji(web_client):
     assert response.data.decode("utf-8") == ":)"
 
 # === End Example Code ===
+
+def test_get_wave(web_client):
+    response = web_client.get('/wave?name=Dana')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'I am waving at Dana'
+
+def test_post_submit(web_client):
+    response = web_client.post('submit', data={'name': 'Dana','message': 'Hello'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Thanks Dana, you sent this message: Hello'
+
+#Excercise tests
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eee" as the body parameter text
+Then: I should get a 200 response with 3 in the message
+"""
+def test_post_count_vowels_eee(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eee'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 3 vowels in "eee"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eunoia" as the body parameter text
+Then: I should get a 200 response with 5 in the message
+"""
+def test_post_count_vowels_eunoia(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eunoia'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 5 vowels in "eunoia"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "mercurial" as the body parameter text
+Then: I should get a 200 response with 4 in the message
+"""
+def test_post_count_vowels_mercurial(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'mercurial'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 4 vowels in "mercurial"'
+
+#Excercise 2 test
+
+def test_sort_names(web_client):
+    response = web_client.post('/sort-names', data={'names': 'Joe,Alice,Zoe,Julia,Kieran'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice,Joe,Julia,Kieran,Zoe'
+
+# # Request:
+# GET /names?add=Eddie
+
+# # This route should return a list of pre-defined names, plus the name given.
+
+# # Expected response (2OO OK):
+# Julia, Alice, Karim, Eddie
+
+#Given name, function returns list with name appended to it.
+
+def test_returns_list_plus_given_name(web_client):
+    response = web_client.get('/names?name=Eddie')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Julia, Alice, Karim, Eddie'
